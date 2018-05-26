d3d94142e6d0c54f747d71270a8f43d927b795d6
Adam <adamnichols1106@gmail.com>
2018-05-26 02:09:13 +0000

    Cleaned up listWifi.py

--- morpheus_core/config/Networking/Wireless/listWifi.py
+++ morpheus_core/config/Networking/Wireless/listWifi.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python
 from wifi import Cell, Scheme
 networkList = Cell.all('wlan0')
-encryption_file = open("./encrypt_data.txt","w")
-address_file = open("./address_data.txt","w")
 i = 0
 while (i < len(networkList)):
-	if (networkList[i].ssid!="The Sonic"):
-		print networkList[i].ssid
-		encryption_file.write(str(networkList[i].encryption_type)+"\n")
-		address_file.write(str(networkList[i].address) + "\n")
+	print networkList[i].ssid
 	i += 1
-encryption_file.close()
