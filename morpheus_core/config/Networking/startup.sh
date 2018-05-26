56946d93266d7dfa928e2072650ea068860fe9ed
Adam <adamnichols1106@gmail.com>
2018-05-26 00:26:10 +0000

    Added network-manager to startup.sh so it doens't conflict with hostapd install.

--- morpheus_core/config/Networking/startup.sh
+++ morpheus_core/config/Networking/startup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
 sleep 30
 sudo iptables-restore < /etc/iptables.def
 sudo udhcpd /etc/udhcpd.conf
+if (( $(dpkg -l | grep -E '^ii' | grep network-manager | wc -l) > "0" )); then
+	echo "network-manager is installed."
+else
+	echo "network-maanger is not installed."
+	sudo apt-get install network-manager -y
+fi
