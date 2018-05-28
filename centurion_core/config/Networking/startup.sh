8c44f78649b32bf7905e33449b4431eee9f181fe
Adam <adamnichols1106@gmail.com>
2018-05-28 17:05:39 +0000

    Continued cleanup

--- centurion_core/config/Networking/startup.sh
+++ centurion_core/config/Networking/startup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 sleep 30
-echo "Startup data at ($date)" > /var/log/startlog.txt
+echo "Startup data at $(date)" > /var/log/startlog.txt
 sudo iptables-restore < /etc/iptables.def
-if (( $dpkg -l | grep -E '^ii' | grep network-manager | wc -l > "0" )); then
+if (( $(dpkg -l | grep -E '^ii' | grep network-manager | wc -l)_ > "0" )); then
 	echo "network-manager is installed." >> /var/log/startlog.txt
 else
 	echo "network-manager is not installed." >> /var/log/startlog.txt
@@ -12,3 +13,4 @@ while (( $(ps -A | grep udhcpd | wc -l) < "1" )); do
 	sudo udhcpd /etc/udhcpd.conf
 	sudo echo "Reran udhcpd" >> /var/log/startlog.txt
 	sleep 1
+done
