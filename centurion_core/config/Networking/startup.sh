a2a8faecbc77bd46fa1c160354b243690d5aa5f9
Adam <adamnichols1106@gmail.com>
2018-05-28 17:12:32 +0000

    Another cleanup commit

--- centurion_core/config/Networking/startup.sh
+++ centurion_core/config/Networking/startup.sh
@@ -2,7 +2,7 @@
 sleep 30
 echo "Startup data at $(date)" > /var/log/startlog.txt
 sudo iptables-restore < /etc/iptables.def
-if (( $(dpkg -l | grep -E '^ii' | grep network-manager | wc -l)_ > "0" )); then
+if (( $(dpkg -l | grep -E '^ii' | grep network-manager | wc -l) > "0" )); then
 	echo "network-manager is installed." >> /var/log/startlog.txt
 else
 	echo "network-manager is not installed." >> /var/log/startlog.txt
