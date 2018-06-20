8caf585767f3aac679e3c166a2d8db3cc24f37d5
Adam <adamnichols1106@gmail.com>
2018-06-20 17:05:06 +0000

    Added Open wifi (opn) to wifiConnect.py

--- centurion_core/config/Networking/Wireless/wifiConnect.py
+++ centurion_core/config/Networking/Wireless/wifiConnect.py
@@ -7,14 +7,14 @@ try:
 	encryption=sys.argv[3]
 except:
 	print "Syntax: \n"
-	print "./wifiConnect.py <ssid> <password> <wpa/wep>"
+	print "./wifiConnect.py <ssid> <password> <wpa/wep/opn>"
 	sys.exit()
 def connect(ssid,password,encryption):
 	if (encryption=="wep"):
 		os.system("nmcli dev wifi connect '"+ssid  + "' wep-key-type "+ password  +" iface wlan0")
 	elif (encryption=="wpa"):
 		os.system("nmcli dev wifi connect '"+ssid + "' password " + password+ " iface wlan0")
-	elif (encryption=="--"):
+	elif (encryption=="opn"):
 		os.system("nmcli dev wifi connect '"+ssid +  "' iface wlan0")
 	else:
 		print "\nInvalid encryption type. Please either type wpa or wep"
