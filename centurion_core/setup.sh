e41d6b9b0536bdb98b61b70e436b07d28a217619
Adam <adamnichols1106@gmail.com>
2018-06-22 15:28:29 +0000

    Fixed traffic forwarding in setup.sh

--- centurion_core/setup.sh
+++ centurion_core/setup.sh
@@ -14,7 +14,7 @@ apt-get install openssh-server -y
 apt install python-pip -y
 pip install wifi
 #Enable packet forwarding for the wireless AP
-echo "net.ipv4.ip_forward = 1" >> /etc/systl.conf
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 #Copy config files for hostapd and udhcpd
 cp ./config/Networking/Access_Point/hostapd.conf /etc/hostapd/hostapd.conf
 cp ./config/Networking/Access_Point/udhcpd.conf /etc/udhcpd.conf
